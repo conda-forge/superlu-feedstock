27ab6b6718d047c4d9a7f3b3d6f43f3c0853f1dd
Min RK <benjaminrk@gmail.com>
2024-11-01 16:04:33 +0100

    7.0.0
    stop specifying several options that are defaults upstream

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,22 +3,16 @@
 mkdir build
 cd build
 
-# enable_blaslib=OFF so OpenBLAS will be found instead of the built-in BLAS
-
 cmake ${CMAKE_ARGS} .. \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" \
-    -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -fPIC" \
-    -Denable_blaslib=OFF \
-    -Denable_single=ON \
-    -Denable_double=ON \
-    -Denable_complex16=ON \
-    -Denable_complex=ON \
+    -DBUILD_SHARED_LIBS=ON \
+    -Denable_internal_blaslib=OFF \
     -Denable_tests=ON \
-    -Denable_doc=OFF \
-    -Denable_matlab_mex=OFF
+    -Denable_examples=OFF \
+    -Denable_doc=OFF
 
-make
+make -j"${CPU_COUNT:-1}"
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
 make test
 fi
